2cf0cd1a7890acea942f20e0dffff86212ee79a2
danigb <danigb@gmail.com>
2016-06-17 13:29:04 +0200

    Simple synth

--- examples/assets/assembler.js
+++ examples/assets/assembler.js
@@ -1,29 +1,79 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict'
 
-function assemble (desc) {
-  desc = desc || {}
-  var ass = ASSEMBLERS[desc.node]
-  if (!ass) return null
-  return function (ac) {
-    var node = ass.assemble(ac, Object.assign({}, ass.defaults, desc))
-    if (desc.connect === true) node.connect(ac.destination)
-    return node
+function oneOf (list) {
+  return function (node, name, value) {
+    if (list.indexOf(value) === -1) {
+      throw Error('Not valid "' + name + '" value: ' + value)
+    }
+    node[name] = value
   }
 }
 
-var ASSEMBLERS = {
+var TYPES = {
+  'a-rate': function (node, name, value) {
+    node[name].value = value
+  },
+  'oscillator-type': oneOf(['sine', 'square', 'sawtooth', 'triangle', 'custom']),
+  'filter-type': oneOf(['lowpass', 'highpass', 'bandpass', 'lowshelf', 'highshelf', 'peaking', 'notch', 'allpass'])
+}
+
+var NODES = {
+  gain: {
+    '_name': 'Gain',
+    gain: 'a-rate'
+  },
   oscillator: {
-    assemble: function (ac, desc) {
-      var node = ac.createOscillator()
-      node.type = desc.type
-      node.frequency.value = desc.frequency
-      return node
-    },
-    defaults: {
-      type: 'square',
-      frequency: 440
-    }
+    '_name': 'Oscillator',
+    frequency: 'a-rate',
+    detune: 'a-rate',
+    type: 'oscillator-type'
+  },
+  filter: {
+    '_name': 'BiquadFilter',
+    frequency: 'a-rate',
+    detune: 'a-rate',
+    Q: 'a-rate',
+    gain: 'a-rate',
+    type: 'filter-type'
   }
+}
+
+function assemble (obj) {
+  return function (ac) {
+    var n = node(ac, obj) || Object.keys(obj).reduce(function (n, k) {
+      n[k] = node(ac, obj[k])
+      return n
+    }, {})
+    makeConnections(ac, n, obj, n)
+    return n
+  }
+}
+
+function makeConnections (ac, node, obj, ctx) {
+  if (!node) return
+  if (obj.connect) {
+    if (obj.connect === '$context') node.connect(ac.destination)
+    else {
+      var dest = ctx[obj.connect]
+      if (dest) node.connect(dest)
+    }
+  } else {
+    Object.keys(node).forEach(function (k) {
+      makeConnections(ac, node[k], obj[k], ctx)
+    })
+  }
+}
+
+function node (ac, obj) {
+  var desc = NODES[obj.node]
+  if (!desc) return null
+  var node = ac['create' + desc['_name']].bind(ac)()
+  Object.keys(obj).forEach(function (k) {
+    var setValue = TYPES[desc[k]]
+    if (setValue) setValue(node, k, obj[k])
+  })
+  return node
 }
 
 var Assembler = { assemble: assemble }
--- test/waa-test.js
+++ test/waa-test.js
@@ -3,9 +3,18 @@ require('web-audio-test-api')
 var test = require('tape')
 var waa = require('..')
 
+test('create gain', function (t) {
+  var ac = new AudioContext()
+  var g = waa.assemble({ node: 'gain' })
+  t.assert(g)
+  t.assert(g(ac))
+  t.assert(g(ac).gain)
+  t.end()
+})
+
 test('create oscillators', function (t) {
   var ac = new AudioContext()
-  var node = waa.assemble({ node: 'oscillator', frequency: 880 })(ac)
+  var node = waa.assemble({ node: 'oscillator', type: 'square', frequency: 880 })(ac)
   t.assert(node)
   t.equal(node.type, 'square')
   t.equal(node.frequency.value, 880)
@@ -14,8 +23,33 @@ test('create oscillators', function (t) {
 
 test('connect to audio context', function (t) {
   var ac = new AudioContext()
-  waa.assemble({ node: 'oscillator', connect: true })(ac)
+  waa.assemble({ node: 'oscillator', connect: '$context' })(ac)
   t.equal(ac.toJSON().inputs.length, 1)
   t.equal(ac.toJSON().inputs[0].name, 'OscillatorNode')
+  t.end()
+})
+
+test('create audio graph', function (t) {
+  var Synth = waa.assemble({
+    name: 'simple-synth',
+    amp: { node: 'gain', connect: '$context' },
+    filter: { node: 'filter', frequency: 400, connect: 'amp' },
+    osc: { node: 'oscillator', connect: 'filter' }
+  })
+  var ac = new AudioContext()
+  var node = Synth(ac)
+  t.assert(node)
+  var next = ac.toJSON()
+  t.equal(next.inputs.length, 1)
+  t.equal(next.inputs[0].name, 'GainNode')
+  next = next.inputs[0]
+  t.equal(next.name, 'GainNode')
+  t.equal(next.inputs.length, 1)
+  next = next.inputs[0]
+  t.equal(next.name, 'BiquadFilterNode')
+  t.equal(next.inputs.length, 1)
+  next = next.inputs[0]
+  t.equal(next.name, 'OscillatorNode')
+  t.equal(next.inputs.length, 0)
   t.end()
 })
